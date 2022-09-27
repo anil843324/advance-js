9e346f3b692e3f3dbedbdfd3b894b7a215ece28e
anil843324 <anilkrsingh14200@gmail.com>
2022-09-27 20:08:37 +0530

    page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </>
   );
